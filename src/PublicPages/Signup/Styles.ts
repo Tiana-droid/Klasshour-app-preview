e3020406f862d2eadb510dfe611ff9592559a49d
Lawson Daniel <lawblaze4@gmail.com>
2022-11-25 21:27:44 +0100

    done with the sign up strcture

---
+++ src/PublicPages/Signup/Styles.ts
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+import { BreakPoints } from "../../utils/breakpoints";
+import { AppColors } from "../../utils/constants";
+
+type LoginPropT = {
+  bg: any;
+};
+export const PageLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+export const BannerCont = styled.div<LoginPropT>`
+  width: 50%;
+  background: url(${(prop) => prop.bg}) no-repeat;
+  background-size: cover;
+  @media (${BreakPoints.xs}) {
+    display: none;
+  }
+  @media (${BreakPoints.large}) {
+    display: block;
+  }
+`;
+
+export const FormCont = styled.div`
+  width: 50%;
+  background: ${AppColors.primary};
+
+  @media (${BreakPoints.xs}) {
+    width: 100%;
+  }
+  @media (${BreakPoints.large}) {
+    width: 50%;
+    padding: 2rem 4rem;
+  }
+`;
+
+export const FormHeader = styled.div`
+  @media (${BreakPoints.xs}) {
+    margin-top: 3rem;
+    padding-left: 2rem;
+  }
+
+  @media (${BreakPoints.large}) {
+    width: 80%;
+    margin: auto;
+  }
+`;
+
+export const FormContainer = styled.div`
+  min-height: 60vh;
+  margin-top: 4rem;
+
+  @media (${BreakPoints.xs}) {
+    width: 90%;
+    margin: 4rem auto;
+  }
+
+  @media (${BreakPoints.large}) {
+    width: 80%;
+    margin: 5rem auto;
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 2rem;
+
+  @media (${BreakPoints.xs}) {
+    h2 {
+      color: #fff;
+      text-align: center;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 2.5rem;
+      line-height: 63px;
+      margin-bottom: 2rem;
+    }
+  }
+
+  @media (${BreakPoints.large}) {
+    h2 {
+      color: #fff;
+      text-align: center;
+      font-style: normal;
+      font-weight: 800;
+      font-size: 54px;
+      line-height: 63px;
+      margin-bottom: 4rem;
+    }
+  }
+`;
+
+export const RoleButton = styled.button`
+  border: 2px solid ${AppColors.brandRed};
+  width: 216px;
+  height: 50px;
+  border-radius: 6px;
+  font-weight: 800;
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
+export const RoleButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
--- src/PublicPages/Signup/index.tsx
+++ src/PublicPages/Signup/index.tsx
@@ -1,9 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
+import SignupBanner from "../../Assets/images/SignupBanner.svg";
+import Logo from "../../Assets/images/Logo.svg";
+import MailIcon from "../../Assets/icons/MailIcon.svg";
+import LockIcon from "../../Assets/icons/LockIcon.svg";
+import UserIcon from "../../Assets/icons/UserIcon.svg";
+import Input from "../../Components/Input";
+import {
+  BannerCont,
+  Form,
+  FormCont,
+  FormContainer,
+  FormHeader,
+  PageLayout,
+  RoleButton,
+  RoleButtonContainer,
+} from "./Styles";
+import { PrimaryBtn } from "../../Components/Button";
+import { toast } from "react-toastify";
+import userOBJ from "../../classes/user.class";
+import { AppColors } from "../../utils/constants";
 
 export default function Signup() {
+  const [isLoading, setisLoading] = useState(false);
+  const [userRole, setuserRole] = useState("Student");
+
+  const handleRegistration = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello");
+
+    //mimick request to klasshour server as example
+    setisLoading(true);
+    setTimeout(() => {
+      toast.success("Registration successful");
+      setisLoading(false);
+    }, 2000);
+  };
+  //handle Role Selection
+  const handleRoleSelection = (role: string) => {
+    console.log(role, "from role selection");
+    setuserRole(role);
+  };
   return (
     <div>
-      <h2>Signup</h2>
+      <PageLayout>
+        <BannerCont bg={SignupBanner}></BannerCont>
+        <FormCont>
+          <FormHeader>
+            <img src={Logo} />
+          </FormHeader>
+          <FormContainer>
+            {/* Todo: Do input validations and connect to state */}
+            <Form>
+              <h2>Welcome</h2>
+              <RoleButtonContainer>
+                <RoleButton
+                  style={{
+                    background: `${
+                      userRole != "Tutor" ? AppColors.primary : "#fff"
+                    } `,
+                    color: `${
+                      userRole != "Tutor" ? "#fff" : AppColors.primary
+                    } `,
+                  }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleRoleSelection("Tutor");
+                  }}
+                >
+                  Tutor
+                </RoleButton>
+                <RoleButton
+                  style={{
+                    background: `${
+                      userRole != "Student" ? AppColors.primary : "#fff"
+                    } `,
+                    color: `${
+                      userRole != "Student" ? "#fff" : AppColors.primary
+                    } `,
+                  }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleRoleSelection("Student");
+                  }}
+                >
+                  Student
+                </RoleButton>
+              </RoleButtonContainer>
+
+              <Input
+                Icon={UserIcon}
+                type="text"
+                placeHolder="Full Name"
+                onChange={() => ""}
+              />
+              <Input
+                Icon={MailIcon}
+                type="email"
+                placeHolder="Email"
+                onChange={() => ""}
+              />
+              <Input
+                Icon={LockIcon}
+                type="password"
+                placeHolder="Enter Password"
+                onChange={() => ""}
+              />
+              <Input
+                Icon={LockIcon}
+                type="password"
+                placeHolder="Confirm password"
+                onChange={() => ""}
+              />
+              <PrimaryBtn
+                isLoading={isLoading}
+                title="Register"
+                onBtnClick={(e: React.FormEvent<HTMLFormElement>) =>
+                  handleRegistration(e)
+                }
+              />
+            </Form>
+          </FormContainer>
+        </FormCont>
+      </PageLayout>
     </div>
   );
 }
