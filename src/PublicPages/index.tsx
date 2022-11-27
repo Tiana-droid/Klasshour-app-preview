0a0da59cd2999fcd62a1529963899e6821f9818f
Lawson Daniel <lawblaze4@gmail.com>
2022-11-27 17:52:08 +0100

    added the reset password link

--- src/PublicPages/index.tsx
+++ src/PublicPages/index.tsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import ErrorPage from "../ErrorPage";
 import Login from "./Login";
 import Signup from "./Signup";
+import ForgetPassword from "./Reset-password/index";
+import ResetPassword from "./Reset-password/ChangePassword";
 
 export default function PublicRoutes() {
   return (
@@ -10,6 +12,8 @@ export default function PublicRoutes() {
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<ErrorPage />} />
+      <Route path="/reset-password" element={<ForgetPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
     </Routes>
   );
 }
