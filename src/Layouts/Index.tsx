86ba772f49e0c4efdd405b4a72cff4c5391d778d
Folexy13 <folajimiopeyemisax13@gmail.com>
2023-01-24 08:45:25 +0100

    ...

--- src/Layouts/Index.tsx
+++ src/Layouts/Index.tsx
@@ -9,7 +9,7 @@ export default function AppLayout() {
   useLayoutEffect(() => {
     if ((getStoredAuthToken() && getStoredClientUser()) || window.location.pathname.split("=").includes('participant&classid')) setisLoggedIn(true);
     console.log(isLoggedIn)
-    console.log("first",window.location.pathname.split("=").includes('participant&classid'))
+    console.log("first",window.location.pathname.split("="))
     if (!getStoredAuthToken() && !getStoredClientUser()) setisLoggedIn(false);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [getStoredAuthToken(), getStoredClientUser()]);
